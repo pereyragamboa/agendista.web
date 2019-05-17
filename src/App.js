eb81a84475f14136631b8ea81dad54e3f7d95e1f
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-05-17 18:32:07 -0500

    Format App component
    First version for the layout of the start page.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hola mundo
-      </header>
+      <div className="columns">
+        <div className="column is-three-fifths-desktop">
+          <h1 className="title">Próximas citas</h1>
+        </div>
+        <div className="column is-two-fifths-desktop tile is-ancestor is-vertical">
+          <p className="button is-primary tile is-child">Citas</p>
+          <p className="button is-primary tile is-child">Servicios</p>
+          <p className="button is-primary tile is-child">Horarios de atención</p>
+        </div>
+      </div>
     </div>
   );
 }
