0864543816b66970638ab04060cd42bcf7da5dfb
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-05-19 21:50:59 -0500

    Add Feather Icons
    (https://feathericons.com/)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import FeatherIcon from './FeatherIcon';
 import './App.css';
 
-function App() {
+function App()
+{
   return (
     <div className="App">
       <div className="columns">
-        <div className="column is-three-fifths-desktop">
+
+        <div className="column is-three-fifths-desktop tile is-ancestor">
           <h1 className="title">Próximas citas</h1>
         </div>
+
         <div className="column is-two-fifths-desktop tile is-ancestor is-vertical">
-          <p className="button is-primary tile is-child">Citas</p>
-          <p className="button is-primary tile is-child">Clientes</p>
-          <p className="button is-primary tile is-child">Servicios</p>
-          <p className="button is-primary tile is-child">Horarios de atención</p>
+          <div className="button is-primary tile is-child">
+            <FeatherIcon iconName='calendar'/>
+            <span>Citas</span>
+          </div>
+          <div className="button is-primary tile is-child">
+            <FeatherIcon iconName='users'/>
+            <p>Clientes</p>
+          </div>
+          <div className="button is-primary tile is-child">
+            <FeatherIcon iconName='shopping-bag'/>
+            <p>Servicios</p>
+          </div>
+          <div className="button is-primary tile is-child">
+            <FeatherIcon iconName='clock'/>
+            <p>Horarios de atención</p>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/FeatherIcon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import feather from 'feather-icons';
+
+const FeatherIcon = (props) => {
+  // Gets icon name from props
+  const iconName = props.iconName;
+
+  // If icon name is not null, set SVG
+  // feather.icons[].toSvg() returns a string, not valid HTML/JSX
+  return <div dangerouslySetInnerHTML={
+    (typeof (iconName) === 'string' && iconName.trim() !== '')
+        ? { __html: feather.icons[iconName.trim()].toSvg(props) }
+        : ''
+  }/>;
+};
+
+
+export default FeatherIcon;
