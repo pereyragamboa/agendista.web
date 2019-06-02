4cdea90d13de152c2e64f1e88cd84845c6401579
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-06-02 15:09:53 -0500

    Add ServiceList

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import getIndex from '../commons/getIndex';
-import AppointmentList from '../appointments/appointmentList';
+import AppointmentList from './serviceList';
 import FeatherIcon from '../commons/featherIcon';
+import getIndex from '../commons/getIndex';
 import * as Paths from "../../paths";
 
 const NavbarEndItems = [
@@ -13,7 +13,7 @@ const NavbarEndItems = [
 ];
 
 export default function Services () {
-  const ServiceIndex = getIndex(<AppointmentList />, { endItems: NavbarEndItems });
+  const ServiceIndex = getIndex(<AppointmentList/>, { endItems: NavbarEndItems });
 
-  return <ServiceIndex brand="Servicios"/>;
+  return <ServiceIndex brand="Servicios" featherIcon="shopping-bag"/>;
 };
---
+++ src/components/services/serviceList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ServiceList() {
+  return <dl>
+    <dt>Servicio 1</dt>
+    <dd>30 minutos - $ 100</dd>
+    <dt>Servicio 2</dt>
+    <dd>1 hora - $ 200</dd>
+    <dt>Servicio 3</dt>
+    <dd>1 hora 30 minutos - $ 300</dd>
+  </dl>
+};
