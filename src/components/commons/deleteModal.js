3faf885c332ef2abc47a4b5e1706943cf4c6648a
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-06-07 15:53:39 -0500

    Add comments

--- src/components/commons/deleteModal.js
+++ src/components/commons/deleteModal.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+// Creates a modal for deleting an element.
+// props:
+// - id: id of the modal; required by showModal component
+// deleteClick: function to be executed when user confirms deletion
+// (presumably it will "delete" an item from a data source)
 export default function DeleteModal(props, deleteClick) {
   function discard() {
     document.getElementById(props.id).classList.remove("is-active");
   }
 
+  // This function wraps the call to deleteClick, adding a previous type check.
+  // This also corrects a warning when deleteClick is directly referenced in
+  // onClick.
   function confirm() {
     if (typeof deleteClick === 'function') {
       deleteClick();
