3a32a3b02e58c2e8f27b7a1761fe56412ef8996d
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-06-18 19:36:02 -0500

    Create footer component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Appointments from './components/appointments';
 import BusinessHours from './components/businessHours';
 import CustomerDetail from './components/customers/customerDetail';
 import Customers from './components/customers';
+import Footer from './components/footer';
 import Services from './components/services';
 import ServiceDetail from './components/services/serviceDetail';
 import Start from './components/start';
@@ -15,19 +16,23 @@ import './App.css';
 function App()
 {
   return (
-    <div className="App">
+    <div className="App hero is-fullheight">
+      <div className="hero-head"/>
       <BrowserRouter>
-        <div>
-          <Route exact path={Paths.HOME} component={Start}/>
-          <Route exact path={Paths.ADD_APPOINTMENT} component={AppointmentDetail}/>
-          <Route exact path={Paths.ADD_CUSTOMER} component={CustomerDetail}/>
-          <Route exact path={Paths.ADD_SERVICE} component={ServiceDetail}/>
-          <Route exact path={Paths.LIST_APPOINTMENTS} component={Appointments}/>
-          <Route exact path={Paths.LIST_CUSTOMERS} component={Customers}/>
-          <Route path={Paths.LIST_HOURS} component={BusinessHours}/>
-          <Route exact path={Paths.LIST_SERVICES} component={Services}/>
+        <div className="hero-body">
+          <div className="container">
+            <Route exact path={Paths.HOME} component={Start}/>
+            <Route exact path={Paths.ADD_APPOINTMENT} component={AppointmentDetail}/>
+            <Route exact path={Paths.ADD_CUSTOMER} component={CustomerDetail}/>
+            <Route exact path={Paths.ADD_SERVICE} component={ServiceDetail}/>
+            <Route exact path={Paths.LIST_APPOINTMENTS} component={Appointments}/>
+            <Route exact path={Paths.LIST_CUSTOMERS} component={Customers}/>
+            <Route path={Paths.LIST_HOURS} component={BusinessHours}/>
+            <Route exact path={Paths.LIST_SERVICES} component={Services}/>
+          </div>
         </div>
       </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Footer() {
+  return <div id="ag-footer" className="hero-foot">
+    <div className="container has-text-centered">
+      <br/>
+      <p>
+        Hecho con&nbsp;
+        <span role="img" aria-label="Corazón">&#x2764;&#xFE0F;</span> y&nbsp;
+        <span role="img" aria-label="Café">&#x2615;</span> por&nbsp;
+        <a href="https://github.com/pereyragamboa">Martín Pereyra</a>
+      </p>
+      <br/>
+    </div>
+  </div>
+};
