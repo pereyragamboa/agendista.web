9e0b7556225d2cf4d77e6c652b650832627acfa7
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-06-24 12:45:11 -0500

    Add ListItemButtons

--- src/components/appointments/appointmentList.js
+++ src/components/appointments/appointmentList.js
@@ -1,22 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import * as Paths from '../../paths';
 import DeleteModal from '../commons/deleteModal';
-import showModal from '../commons/showModal';
-import FeatherIcon from '../commons/featherIcon';
+import ListItemButtons from '../commons/listItemButtons';
 
 const MODAL_ID = "ag-deleteAppointmentModal";
 
 // Row of an appointment list.
 const AppointmentListElement = (props) =>
   <tr>
-    <th>{props.time}</th>
+    <th><Link to={Paths.ADD_APPOINTMENT}>{props.time}</Link></th>
     <td>{props.name}</td>
     {
       props.showButtons ? (
-          <td className="level-right">
-            <div className="buttons level-item">
-              <button className="button is-primary"><FeatherIcon iconName="edit-2"/></button>
-              <button className="button is-danger" onClick={() => showModal(MODAL_ID)}><FeatherIcon iconName="trash-2"/></button>
-            </div>
+          <td>
+            <ListItemButtons editPath={Paths.ADD_APPOINTMENT} deleteModalId={MODAL_ID}/>
           </td>) : ''
     }
   </tr>
