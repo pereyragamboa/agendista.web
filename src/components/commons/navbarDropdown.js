816c04344d4b5f2c689742753690542ac033e5dc
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-07-05 19:25:17 -0500

    Handle dropdown with onClick()
    CSS class 'is-hoverable' replaced with click(); it was done as a way for fixing a problem of the dropdown menu; I believe the problem is related to the existence of two
    navbar items in the same space

--- src/components/commons/navbarDropdown.js
+++ src/components/commons/navbarDropdown.js
@@ -7,14 +7,22 @@ import NavbarMenuItem from './navbarMenuItem';
  * @param props.featherIcon Name of Feather icon
  * @param props.caption Text of dropdown
  * @param props.children Zero or more NavbarMenuItems
+ * @param props.id Identifier of the dropdown.
  * @return {*}
  * @constructor
  */
 export default function NavbarDropdown(props) {
-  const {featherIcon, caption, ...otherProps} = props;
-  const className=getClassName("navbar-item has-dropdown is-hoverable", props);
+  const {featherIcon, caption, id, ...otherProps} = props;
+  const className = getClassName("navbar-item navbar-link has-dropdown", props);
+  // If props.id is not provided, use current time
+  const dropdownId = `ag-navbar-dropdown-${id || Date.now()}`;
 
-  return <div className={className} {...otherProps}>
+  function onDropdownClick() {
+    console.log(dropdownId);
+    document.getElementById(dropdownId).classList.toggle("is-active");
+  }
+
+  return <div id={dropdownId} className={className} {...otherProps} onClick={onDropdownClick}>
     <NavbarMenuItem featherIcon={featherIcon} caption={caption}/>
     <div className="navbar-dropdown">
       {props.children}
