c278326cf9830fab5a21c44355a6769cbee387cb
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-07-10 12:24:31 -0500

    Add tests for capitalize()

--- src/utilities/capitalize.test.js
+++ src/utilities/capitalize.test.js
@@ -7,10 +7,22 @@ describe("capitalize() tests", () => {
   test("capitalize 'xyz'", () => {
     expect(capitalize("xyz")).toBe("Xyz");
   });
+  test("capitalize single character", () => {
+    expect(capitalize("f")).toBe("F");
+  });
   test("do nothing to 'KLM'", () => {
     expect(capitalize("KLM")).toBe("KLM");
   });
-  test("capitalize 'ñññ'", () => {
+  test("do nothing to non-text string", () => {
+    expect(capitalize("123")).toBe("123");
+  });
+  test("capitalize diacritics", () => {
     expect(capitalize("ñññ")).toBe("Ñññ");
-  })
+  });
+  test("capitalize Cyrillic string", () => {
+    expect(capitalize("кириллица")).toBe("Кириллица")
+  });
+  test("capitalize Greeek string", () => {
+    expect(capitalize("ελληνικό")).toBe("Ελληνικό")
+  });
 });
