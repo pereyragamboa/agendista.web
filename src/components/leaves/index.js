f9187bd0dee9b636d49810066bde67d8314f686b
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-11-29 16:54:48 -0600

    Add query to Leave component

--- src/components/leaves/index.js
+++ src/components/leaves/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 import { DELETE_LEAVE_MODAL } from "../../constants/modalIds";
 import { LEAVE } from "../../constants/headers";
 import { ADD_LEAVE, UPDATE_LEAVE } from "../../constants/paths";
 import DeleteModal from '../commons/modals/deleteModal';
+import ErrorPanel from '../commons/errorPanel';
 import getIndex from '../commons/getIndex';
+import listGraphQLErros from '../commons/listGraphQLErrors';
 import ListItemButtons from '../commons/listItemButtons';
+import LoadingPanel from '../commons/loadingPanel';
 import NavbarMenuItem from '../commons/navbars/navbarMenuItem'
 
 function LeaveRow (props) {
@@ -37,31 +42,40 @@ function LeaveRow (props) {
   </tr>;
 }
 
-export default class LeaveList extends React.Component {
-  render () {
-    const listBody = <React.Fragment>
-      <p>Los clientes no podrán agendar citas durante estos periodos.</p>
-      <table className="table is-fullwidth">
-        <tbody className="table-container">
-          <tr>
-            <th className="is-hidden-desktop">Periodo</th>
-            <th className="is-hidden-touch">Desde</th>
-            <th className="is-hidden-touch">Hasta</th>
-            <th/>
-          </tr>
-          <LeaveRow fromDate={new Date(2019, 11, 1)} toDate={new Date(2019, 11, 10)}/>
-          <LeaveRow fromDate={new Date(2020, 4, 20)} toDate={new Date(2020, 5, 5)}/>
-        </tbody>
-      </table>
-      <DeleteModal id={DELETE_LEAVE_MODAL}>¿Desea eliminar este periodo vacacional?</DeleteModal>
-    </React.Fragment>;
+export default function LeaveList (props) {
+  const { loading, error, data } = useQuery(gql`
+    query { getLeaves(profileId: "0x30001") {
+        id
+        from
+        to
+    }}
+  `);
+  if (loading) return <LoadingPanel subject={LEAVE}/>;
+  if (error) return <ErrorPanel>{listGraphQLErros(error)}</ErrorPanel>;
+  const listBody = <React.Fragment>
+    <p>Los clientes no podrán agendar citas durante estos periodos.</p>
+    <table className="table is-fullwidth">
+      <tbody className="table-container">
+        <tr>
+          <th className="is-hidden-desktop">Periodo</th>
+          <th className="is-hidden-touch">Desde</th>
+          <th className="is-hidden-touch">Hasta</th>
+          <th/>
+        </tr>
+        {
+          data.getLeaves.map(leave =>
+              <LeaveRow key={`key${leave.id}`} fromDate={new Date(leave.from)} toDate={new Date(leave.to)}/>)
+        }
+      </tbody>
+    </table>
+    <DeleteModal id={DELETE_LEAVE_MODAL}>¿Desea eliminar este periodo vacacional?</DeleteModal>
+  </React.Fragment>;
 
-    const navbarItems = {
-      endItems: <NavbarMenuItem caption="Nuevo periodo vacacional" featherIcon="plus" path={ADD_LEAVE}/>
-    };
+  const navbarItems = {
+    endItems: <NavbarMenuItem caption="Nuevo periodo vacacional" featherIcon="plus" path={ADD_LEAVE}/>
+  };
 
-    const LeaveIndex = getIndex(listBody, navbarItems);
+  const LeaveIndex = getIndex(listBody, navbarItems);
 
-    return <LeaveIndex {...this.props} brand={LEAVE} featherIcon="sun"/>;
-  }
+  return <LeaveIndex {...props} brand={LEAVE} featherIcon="sun"/>;
 }
