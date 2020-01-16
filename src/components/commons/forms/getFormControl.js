80932daeddc0bbb2363956860c47c92ff12b23fc
Martin Pereyra <pereyra.gamboa@outlook.com>
2020-01-16 13:42:38 -0600

    Add Bulma add-ons to getFormControl()

--- src/components/commons/forms/getFormControl.js
+++ src/components/commons/forms/getFormControl.js
@@ -4,8 +4,9 @@ import getClassName from '../../../utilities/getClassName';
 
 /**
  * Wraps a form input element into a Bulma-styled field, decorated with Feather icons.
+ * If the component has children, they will be shown as addons to the main input element.
  *
- * @param inputElement <input> element to be embedded.
+ * @param InputElement <input> element to be embedded.
  * @return {*}
  * @constructor
  */
@@ -13,14 +14,32 @@ export default function getFormControl(InputElement) {
   return class extends React.Component {
     render() {
       const {caption, iconName, ...otherProps} = this.props;
-      const controlClassName = ["control", (iconName ? "has-icons-left" : '')].join(' ');
+      const hasChildren = this.props.children;
+      const fieldClassName = [
+        "field",
+        (hasChildren ? "has-addons" : '')
+      ].join(' ');
+      const controlClassName = [
+          "control",
+          (iconName ? "has-icons-left" : ''),
+          (hasChildren ? "is-expanded" : '')
+      ].join(' ');
+      const captionElement = caption ? (
+          hasChildren ?
+              <div className="control"><button className="button is-static">{caption}</button></div> :
+              <label className="label">{caption}</label>
+          ) : "";
+      const addOns = hasChildren ?
+          <div className="control">{this.props.children}</div> :
+          "";
 
-      return <div className={getClassName("field", this.props)}>
-        <label className="label">{caption}</label>
+      return <div className={getClassName(fieldClassName, this.props)}>
+        {captionElement}
         <div className={controlClassName}>
           <FeatherIcon iconName={iconName}/>
           <InputElement {...otherProps}/>
         </div>
+        {addOns}
       </div>;
     }
   }
