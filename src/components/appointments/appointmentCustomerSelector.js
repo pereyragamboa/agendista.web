44520b68c47d74888aae9f3d9851faef0c8275d4
Martin Pereyra <pereyra.gamboa@outlook.com>
2020-01-20 17:43:00 -0600

    Add onchange handler to customer search input

--- src/components/appointments/appointmentCustomerSelector.js
+++ src/components/appointments/appointmentCustomerSelector.js
@@ -2,13 +2,27 @@ import React from 'react';
 import FeatherInput from '../commons/forms/featherInput';
 import FeatherIcon from "../commons/featherIcon";
 
+let timer = null; // Timer reference
+
+function onCustomerChange(e) {
+  if (timer != null) {
+    // Resets timer
+    clearTimeout(timer);
+    timer = null;
+  }
+  // Reassigns timer
+  timer = setTimeout(x => console.log(x), 1000, e.target.value);
+}
+
 export default function AppointmentCustomerSelector() {
   return <React.Fragment>
-    <FeatherInput iconName="user" placeholder="Nombre(s)" caption="Agendar la cita a:">
+    <FeatherInput iconName="user" placeholder="Nombre(s)" caption="Agendar la cita a:" onChange={onCustomerChange}>
       <button className="button is-primary">
         <FeatherIcon iconName="search"/>
       </button>
     </FeatherInput>
+    <table className="table">
+    </table>
     <button className="button is-success">
       <FeatherIcon iconName="arrow-right"/>
       <span>Siguiente</span>
