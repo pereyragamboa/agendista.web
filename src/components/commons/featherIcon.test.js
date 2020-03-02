3ce8741f7f3604ac82a2b63ed3c8ae7192bf22a3
Martin Pereyra <pereyra.gamboa@outlook.com>
2020-03-02 12:05:48 -0600

    Update snapshot

--- src/components/commons/featherIcon.test.js
+++ src/components/commons/featherIcon.test.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import TestRenderer from 'react-test-renderer';
 import FeatherIcon from './featherIcon';
+import TestContainer from '../testHelpers/testContainer';
 
 describe("<FeatherIcon> tests", () => {
-  test("Renders component", () => {
-    const render = TestRenderer.create(<FeatherIcon iconName="feather"/>);
-    expect(render.toJSON()).toMatchSnapshot();
+  describe("Renders component", () => {
+    test("Matches snapshot", () => {
+      const testRender = TestRenderer.create(<FeatherIcon iconName="feather"/>);
+      expect(testRender.toJSON()).toMatchSnapshot();
+    });
+
+    test("Contains an icon", () => {
+      const testContainer = new TestContainer();
+      testContainer.createContainer();
+      act(() => {
+        render(<FeatherIcon iconName="feather"/>, testContainer.getContainer());
+      });
+      expect(testContainer.getContainer().getElementsByTagName("svg").length).toBe(1);
+    });
   });
+  test.skip("Throws error at empty component", () => {
+    const render = TestRenderer.create(<FeatherIcon iconName="fnord"/>);
+    expect()
+  })
 });
