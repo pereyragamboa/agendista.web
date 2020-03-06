6ed31ef87149589808e10dccf75d14bbb604a714
Martin Pereyra <pereyra.gamboa@outlook.com>
2020-03-06 15:23:12 -0600

    Add <DeleteModal> unit tests

--- src/components/commons/modals/deleteModal.js
+++ src/components/commons/modals/deleteModal.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+export const Ids = {
+  YES_BUTTON: "ag-delete-modal-yes-button",
+  NO_BUTTON: "ag-delete-modal-no-button"
+};
+
 /** Creates a modal for deleting an element.
  *
  * @param props.children Content of the modal; if not specified shows a default message
@@ -8,12 +13,22 @@ import React from 'react';
  * @constructor
  */
 export default function DeleteModal(props) {
+  if (!props.id) throw new Error("Missing ID from <DeleteModal>.");
+
+  /**
+   * Discards and hides the modal.
+   */
   function discard() {
     document.getElementById(props.id).classList.remove("is-active");
   }
-  // This function wraps the call to props.delete, adding a previous type check.
-  // This also corrects a warning raised when props.delete is directly referenced in
-  // onClick.
+
+  /**
+   * Executes the delete action for which the modal was requesting confirmation.
+   *
+   * This function wraps the call to props.delete, adding a previous type check.
+   * This also corrects a warning raised when props.delete is directly referenced in
+   * onClick.
+   */
   function confirm() {
     if (typeof props.delete === 'function') {
       props.delete();
@@ -27,8 +42,8 @@ export default function DeleteModal(props) {
     <div className="modal-content box">
       <div className="content">{content}</div>
       <div className="buttons">
-        <button className="button is-danger" onClick={confirm}>Sí</button>
-        <button className="button is-primary" onClick={discard}>No</button>
+        <button id={Ids.YES_BUTTON} className="button is-danger" onClick={confirm}>Sí</button>
+        <button id={Ids.NO_BUTTON} className="button is-primary" onClick={discard}>No</button>
       </div>
     </div>
   </div>;
---
+++ src/components/commons/modals/deleteModal.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import DeleteModal, { Ids } from './deleteModal';
+import TestContainer from '../../testHelpers/testContainer';
+
+describe("<DeleteModal> tests", () => {
+  const testContainer = new TestContainer();
+
+  describe("Modal buttons", () => {
+    const ID = "test-modal";
+    const mockDelete = jest.fn();
+
+    beforeEach(() => {
+      testContainer.createContainer();
+      act(() => {
+        render(<DeleteModal id={ID} delete={mockDelete}/>, testContainer.getContainer());
+      });
+    });
+
+    afterEach(testContainer.disposeContainer);
+
+    test("confirms modal", () => {
+      const yesButton = document.getElementById(Ids.YES_BUTTON);
+      yesButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+      expect(mockDelete).toHaveBeenCalled();
+    });
+
+    test("discards modal", () => {
+      const noButton = document.getElementById(Ids.NO_BUTTON);
+      noButton.dispatchEvent(new MouseEvent("click", {bubbles: true}));
+      expect(document.getElementById(ID).classList.contains("is-active")).toBeFalsy();
+    });
+  });
+
+  test("Modal with no ID", () => {
+    testContainer.createContainer();
+    expect(() => {
+        render(<DeleteModal/>, testContainer.getContainer());
+    }).toThrow();
+    testContainer.disposeContainer();
+  });
+});
