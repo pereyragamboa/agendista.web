7384a623150105b6d9dd84c8c34d63549386a29a
Martin Pereyra <pereyra.gamboa@outlook.com>
2020-04-01 17:42:17 -0600

    Add loading panel ID

--- src/components/commons/loadingPanel.js
+++ src/components/commons/loadingPanel.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+export const LOADING_PANEL_ID = "ag-loading-panel";
+
 /**
  * Shows a loading panel.
  *
@@ -8,7 +10,7 @@ import React from 'react';
  * @constructor
  */
 export default function LoadingPanel(props) {
-  return <div>
+  return <div id={LOADING_PANEL_ID}>
     <h3 className={"title is-4 has-text-centered"}>Buscando{props.subject ? ' ' + props.subject.toLowerCase() : ''}...</h3>
     <progress className={"progress is-primary"} max={100}/>
   </div>
