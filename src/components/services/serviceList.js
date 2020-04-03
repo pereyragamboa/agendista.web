4a43ad721a5164082e006c53408fa0300852b287
Martin Pereyra <pereyra.gamboa@outlook.com>
2020-04-03 17:28:14 -0600

    Add classes

--- src/components/services/serviceList.js
+++ src/components/services/serviceList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 import * as Paths from '../../constants/paths';
 import { DELETE_SERVICE_MODAL as MODAL_ID } from "../../constants/modalIds";
@@ -10,6 +9,15 @@ import ListItemButtons from '../commons/listItemButtons';
 import listGraphQLErrors from '../commons/listGraphQLErrors';
 import LoadingPanel from '../commons/loadingPanel';
 import { SERVICES } from '../../constants/headers';
+import { GET_ALL_SERVICES } from "../../data/queries/serviceQueries";
+
+export const ClassNames = {
+  SERVICE_LIST: "ag-service-list",
+  SERVICE_LIST_ITEM: "ag-service-list-item",
+  SERVICE_LIST_NAME_LABEL: "ag-service-list-item-name",
+  SERVICE_LIST_TIME_LABEL: "ag-service-list-item-time",
+  SERVICE_LIST_PRICE_LABEL: "ag-service-list-item-price"
+};
 
 /**
  * Row element showing service information.
@@ -21,10 +29,12 @@ import { SERVICES } from '../../constants/headers';
  * @constructor
  */
 const ServiceListRow = (props) =>
-  <tr>
-    <th>{ props.serviceName }</th>
-    <td className="is-hidden-touch">{ props.serviceTime }</td>
-    <td className="is-hidden-touch">{ props.servicePrice }</td>
+  <tr className={ClassNames.SERVICE_LIST_ITEM}>
+    <th className={ClassNames.SERVICE_LIST_NAME_LABEL}>{ props.serviceName }</th>
+    <td className={`is-hidden-touch ${ClassNames.SERVICE_LIST_TIME_LABEL}`}>
+      { props.serviceTime }</td>
+    <td className={`is-hidden-touch ${ClassNames.SERVICE_LIST_PRICE_LABEL}`}>
+      { props.servicePrice }</td>
     <td>
       <div className="level-right">
         <ListItemButtons editPath={Paths.UPDATE_SERVICE + props.id} deleteModalId={MODAL_ID}/>
@@ -39,19 +49,11 @@ const ServiceListRow = (props) =>
  * @constructor
  */
 export default function ServiceList() {
-  const { loading, error, data } = useQuery(gql`{
-      getServices(profileId: "0x30001") {
-          id
-          name
-          description
-          duration
-          price
-      }
-  }`);
+  const { loading, error, data } = useQuery(GET_ALL_SERVICES);
   if (loading) return <LoadingPanel subject={SERVICES}/>;
   if (error) return <ErrorPanel>{listGraphQLErrors(error)}</ErrorPanel>;
-  return <React.Fragment>
-    <table className="table is-fullwidth is-hoverable">
+  return <>
+    <table className={`table is-fullwidth is-hoverable ${ClassNames.SERVICE_LIST}`}>
       <thead>
         <tr>
           <th>Servicio</th>
@@ -74,5 +76,5 @@ export default function ServiceList() {
     <DeleteModal id={MODAL_ID}>
       ¿Desea eliminar este servicio?
     </DeleteModal>
-  </React.Fragment>
+  </>
 };
