f070c85f227edbda5ecf72d10b362f0b49d326d4
Martin Pereyra <pereyra.gamboa@outlook.com>
2020-04-13 19:44:20 -0500

    Add class name and id

--- src/components/commons/editLink.js
+++ src/components/commons/editLink.js
@@ -5,10 +5,11 @@ import FeatherIcon from './featherIcon';
 /**
  * Creates a decorated link for editing. The link has a small pencil icon to its right.
  * @param props.to Destination URL; ideally a form.
+ * @param props.id Identifier of the element.
  * @return {*}
  */
 export default (props) => <Link to={props.to}>
   <FeatherIcon iconName="edit-2" width="12" height="12"/>
-  <span>{props.children}</span>
+  <span className={props.className} id={props.id}>{props.children}</span>
 </Link>;
 
