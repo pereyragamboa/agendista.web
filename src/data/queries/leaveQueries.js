b8e806e6f86c4e7ec644f7a4eab3fdaae9a579b3
Martin Pereyra <pereyra.gamboa@outlook.com>
2020-06-04 20:04:24 -0500

    Fix leave mutations

--- src/data/queries/leaveQueries.js
+++ src/data/queries/leaveQueries.js
@@ -14,3 +14,23 @@ export const GET_LEAVE = gql`{
         to
     }
 }`;
+
+export const ADD_LEAVE = gql`
+    mutation ($from: Date!, $to: Date!) { addLeave (
+        profileId: "0x30001"
+        leave: {
+            from: $from,
+            to: $to
+        }
+    ) { id }
+}`;
+
+export const UPDATE_LEAVE = gql`
+    mutation ($id: ID!, $from: Date!, $to: Date!) { updateLeave (
+        leaveId: $id
+        leave: {
+            from: $from,
+            to: $to
+        }
+    )}
+`;
